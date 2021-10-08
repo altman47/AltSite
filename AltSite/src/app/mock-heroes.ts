5d278f8583ee93cb1b58d5607751df9915fd7905
Joe Altmann <jaltmann89@gmail.com>
2021-10-08 15:10:38 -0400

    Initial Sync

---
+++ AltSite/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: "Mercy"},
+    { id: 12, name: "Winston"},
+    { id:13, name: "Hero"},
+    { id:14, name: "Echo"},
+    { id:15, name: "Moira"}
+]
